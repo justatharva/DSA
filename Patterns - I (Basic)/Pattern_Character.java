58737ab45fffdf09c7f0865444963f2d1dcbaba7
justatharva <atharvaking2080@gmail.com>
2026-05-23 22:13:36 +0530

    Character Pattern

---
+++ Patterns - I (Basic)/Pattern_Character.java
@@ -0,0 +1,24 @@
+// Character Pattern
+/*
+
+A
+BC
+DEF
+GHIJ
+
+*/
+public class Pattern_Character {
+    public static void main(String[] args) {
+        
+        char ch = 'A';
+        int n = 4;
+    
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
